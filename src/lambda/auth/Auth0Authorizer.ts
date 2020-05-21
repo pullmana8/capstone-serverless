9cd3494fbce581ee11fceb2a4eb0e5914354bf7e
pullmana8 <pullmana8@gmail.com>
2020-05-21 12:33:52 -0500

    Testing JwtToken

--- src/lambda/auth/Auth0Authorizer.ts
+++ src/lambda/auth/Auth0Authorizer.ts
@@ -1,14 +1,9 @@
 import { Logger } from '@sailplane/logger'
-import {
-  CustomAuthorizerEvent,
-  CustomAuthorizerResult,
-  CustomAuthorizerHandler,
-} from 'aws-lambda'
 import { verify } from 'jsonwebtoken'
-import * as AWS from 'aws-sdk'
-import { JwtToken } from '../../token/JwtToken'
+import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { secretsManager } from 'middy/middlewares'
+import { JwtPayload } from '../../token/JwtPayload'
 
 const logger = new Logger('auth-user')
 
@@ -16,51 +11,56 @@ const logger = new Logger('auth-user')
 const secretId = process.env.AUTH_0_SECRET_ID
 const secretField = process.env.AUTH_0_SECRET_FIELD
 
-const client = new AWS.SecretsManager()
+/* const client = new AWS.SecretsManager()
 
-/* Cache secret if a Lambda is reused */
-let cachedSecret: string
+Cache secret if a Lambda is reused
+let cachedSecret: string */
 
-export const handler: CustomAuthorizerHandler = async (
-  event: CustomAuthorizerEvent,
-): Promise<CustomAuthorizerResult> => {
-  try {
-    const decodedToken = await verifyToken(event.authorizationToken!)
-    logger.infoObject('User was authorized', decodedToken)
+export const handler = middy(async (event: CustomAuthorizerEvent, context): Promise<CustomAuthorizerResult> => {
+    try {
+      const decodedToken = await verifyToken(
+        event.authorizationToken,
+        context.AUTH0SECRET[secretField!],
+      )
+      logger.infoObject('User was authorized', decodedToken)
 
-    return {
-      principalId: decodedToken.sub,
-      policyDocument: {
-        Version: '2012-10-17',
-        Statement: [
-          {
-            Action: 'execute-api:Invoke',
-            Effect: 'Allow',
-            Resource: '*',
-          },
-        ],
-      },
+      return {
+        principalId: decodedToken.sub,
+        policyDocument: {
+          Version: '2012-10-17',
+          Statement: [
+            {
+              Action: 'execute-api:Invoke',
+              Effect: 'Allow',
+              Resource: '*',
+            },
+          ],
+        },
+      }
+    } catch (e) {
+      logger.info('User was not authorized', e.message)
+
+      return {
+        principalId: 'user',
+        policyDocument: {
+          Version: '2012-10-17',
+          Statement: [
+            {
+              Action: 'execute-api:Invoke',
+              Effect: 'Deny',
+              Resource: '*',
+            },
+          ],
+        },
+      }
     }
-  } catch (e) {
-    logger.info('User was not authorized', e.message)
+  },
+)
 
-    return {
-      principalId: 'user',
-      policyDocument: {
-        Version: '2012-10-17',
-        Statement: [
-          {
-            Action: 'execute-api:Invoke',
-            Effect: 'Deny',
-            Resource: '*',
-          },
-        ],
-      },
-    }
-  }
-}
-
-async function verifyToken(authHeader: string): Promise<JwtToken> {
+async function verifyToken(
+  authHeader: string,
+  secret: string,
+): Promise<JwtPayload> {
   if (!authHeader) throw new Error('No authorization error')
 
   if (!authHeader.toLowerCase().startsWith('bearer '))
@@ -69,14 +69,14 @@ async function verifyToken(authHeader: string): Promise<JwtToken> {
   const split = authHeader.split(' ')
   const token = split[1]
 
-  const secretObject: any = await getSecret()
-  const secret = secretObject[secretField!]
+  /*  const secretObject: any = await getSecret()
+  const secret = secretObject[secretField!] */
 
-  return verify(token, secret) as JwtToken
+  return verify(token, secret) as JwtPayload
   /*  if (token !== '123') throw new Error('Invalid token'); */
 }
 
-async function getSecret() {
+/* async function getSecret() {
   if (cachedSecret) return cachedSecret
 
   const data = await client
@@ -88,4 +88,15 @@ async function getSecret() {
   cachedSecret = data.SecretString!
 
   return JSON.parse(cachedSecret)
-}
+} */
+
+handler.use(
+  secretsManager({
+    cache: true,
+    cacheExpiryinMillis: 60000,
+    throwonFailedCall: true,
+    secrets: {
+      AUTH0SECRET: secretId,
+    },
+  }),
+)
